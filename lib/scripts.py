1cd94a0ff1aedce0fed5f7d8f8d932d3168322af
waldest <waldemar.stal@gmail.com>
2014-05-20 23:28:42 +0200

    add exception and add opener file

--- lib/scripts.py
+++ lib/scripts.py
@@ -1,15 +1,21 @@
 #-*- coding: utf-8 -*-
+import sys
 from optparse import OptionParser
 parser = OptionParser()
 (options, args) = parser.parse_args()
+from parser import run_parser
 
 
 def main():
     if len(args) != 1:
         print 'Incorrect number of arguments! Try again.'
-    else:
-        name = args[0]
-        print name
+        sys.exit(0)
+    file_name = args[0]
+    try:
+        run_parser(file_name)
+    except Exception as e:
+        print '%s!' % e
+
 
 if __name__ == '__main__':
     main()

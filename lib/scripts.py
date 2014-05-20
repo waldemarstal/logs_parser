b9b0b46ef0e95049c02bae4c626345a8fc14e051
waldest <waldemar.stal@gmail.com>
2014-05-20 22:18:06 +0200

    add parser to argument from command line

--- lib/scripts.py
+++ lib/scripts.py
@@ -1,5 +1,15 @@
 #-*- coding: utf-8 -*-
+from optparse import OptionParser
+parser = OptionParser()
+(options, args) = parser.parse_args()
 
 
 def main():
-    print 'hello'
+    if len(args) != 1:
+        print 'Incorrect number of arguments! Try again.'
+    else:
+        name = args[0]
+        print name
+
+if __name__ == '__main__':
+    main()

19578d49f01f23a499307f6f121f95afb873e491
waldest <waldemar.stal@gmail.com>
2014-05-21 16:47:09 +0200

    add possible select tld to find

--- lib/parser.py
+++ lib/parser.py
@@ -10,10 +10,12 @@ class ParseError(Exception):
 
 class Parser():
 
-    def __init__(self, file_name):
+    def __init__(self, file_name, tld):
         self.file_name = file_name
-        self.probability_tree = {'pl': {'url_list': []}}
+        self.tld = tld
+        self.probability_tree = {self.tld: {'url_list': []}}
         self.fd = ''
+        self.res = ''
         self.url_in_text = re.compile(ur'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
 
     @staticmethod
@@ -22,11 +24,10 @@ class Parser():
         parse = parse.geturl().split('?')[0]
         return parse
 
-    @staticmethod
-    def is_selected_tld(parse):
+    def is_selected_tld(self, parse):
         parse = urlparse(parse)
         domain = parse.netloc.split('.')[-1]
-        if domain == 'pl':
+        if domain == self.tld:
             return True
         return False
 
@@ -70,10 +71,10 @@ class Parser():
     def _create_probability_tree(self, parse_url):
         parse_url = urlparse(parse_url)
         before_domain = parse_url.netloc.split('.')[-2]
-        if not self.probability_tree['pl'].get(before_domain):
-            self.probability_tree['pl'][before_domain] = {}
-        self.probability_tree['pl']['url_list'].append(parse_url.geturl())
-        value = self.probability_tree['pl'][before_domain]
+        if not self.probability_tree[self.tld].get(before_domain):
+            self.probability_tree[self.tld][before_domain] = {}
+        self.probability_tree[self.tld]['url_list'].append(parse_url.geturl())
+        value = self.probability_tree[self.tld][before_domain]
         path_list = self.create_path_list(parse_url.path)
         for elem in path_list:
             if elem not in value:
@@ -81,15 +82,15 @@ class Parser():
                 value = value[elem]
             else:
                 value = value[elem]
-        if not self.probability_tree['pl'][before_domain].get('path_list'):
-            self.probability_tree['pl'][before_domain]['path_list'] = []
-        self.probability_tree['pl'][before_domain]['path_list'].append(
+        if not self.probability_tree[self.tld][before_domain].get('path_list'):
+            self.probability_tree[self.tld][before_domain]['path_list'] = []
+        self.probability_tree[self.tld][before_domain]['path_list'].append(
             parse_url.path
         )
 
     @staticmethod
     def create_path_list(parse_url):
-        if parse_url == '/':
+        if parse_url == '/' or not parse_url:
             return []
         if parse_url[0] == '/':
             parse_url = parse_url[1:]
@@ -98,8 +99,8 @@ class Parser():
         return parse_url.split('/')
 
     def calculate_probability(self):
-        divisor = len(self.probability_tree['pl'])
-        for k, v in self.probability_tree['pl'].iteritems():
+        divisor = len(self.probability_tree[self.tld])
+        for k, v in self.probability_tree[self.tld].iteritems():
             if type(v) is not dict:
                 continue
             for path in v['path_list']:
@@ -120,11 +121,11 @@ class Parser():
 
     def find_count_of_member(self, param, bdom):
         count = 0
-        for k, v in self.probability_tree['pl'].iteritems():
+        for k, v in self.probability_tree[self.tld].iteritems():
             last = '###@'
             if k == bdom:
                 continue
-            value = self.probability_tree['pl'][k]
+            value = self.probability_tree[self.tld][k]
             for elem in param:
                 if elem not in value:
                     break
--- lib/scripts.py
+++ lib/scripts.py
@@ -7,12 +7,12 @@ from parser import Parser
 
 
 def main():
-    if len(args) != 1:
+    if len(args) != 2:
         print 'Incorrect number of arguments! Try again.'
         sys.exit(0)
-    file_name = args[0]
+    file_name, tld = args[0], args[1]
     try:
-        logs_parser = Parser(file_name)
+        logs_parser = Parser(file_name, tld)
         logs_parser.run_parser()
     except Exception as e:
         print '%s!' % e
